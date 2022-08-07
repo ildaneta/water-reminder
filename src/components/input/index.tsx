4f551026b6087067f2b273405db0d5835d1994f0
ildaneta <iildaemanoely@gmail.com>
2022-08-07 16:00:17 -0300

    feat: create input component and its styles

---
+++ src/components/input/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TextInputProps,
+} from 'react-native';
+
+import {styles} from './styles';
+
+interface IInputProps extends TextInputProps {
+  label: string;
+  icon?: React.ReactNode;
+  iconPress?: () => void;
+}
+
+const Input = ({icon, label, iconPress, ...rest}: IInputProps): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.doubleContainer} />
+      <View style={styles.containerInput}>
+        <View style={styles.containerLabelText}>
+          <Text style={styles.label}>{label}</Text>
+          <TextInput style={styles.input} {...rest} />
+        </View>
+
+        {icon && (
+          <TouchableOpacity
+            style={styles.icon}
+            activeOpacity={0.8}
+            onPress={iconPress}>
+            {icon}
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Input;
---
+++ src/components/input/styles.ts
@@ -0,0 +1,57 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    marginRight: 6,
+  },
+
+  doubleContainer: {
+    width: '100%',
+    height: 50,
+    borderColor: '#111',
+    borderWidth: 2,
+    paddingHorizontal: 16,
+    borderRadius: 9,
+    backgroundColor: '#59F4DB',
+    position: 'absolute',
+    top: 7,
+    left: 6,
+  },
+
+  containerInput: {
+    flexDirection: 'row',
+    backgroundColor: '#fdfdfd',
+    height: 50,
+    borderColor: '#111',
+    borderWidth: 2,
+    borderRadius: 9,
+  },
+
+  containerLabelText: {
+    flexDirection: 'row',
+    width: '90%',
+    alignItems: 'center',
+  },
+
+  icon: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  label: {
+    width: '22%',
+    marginRight: 5,
+    marginLeft: 16,
+    color: '#666',
+    fontWeight: '700',
+  },
+
+  input: {
+    width: '70%',
+    marginRight: 100,
+    color: '#333',
+    fontWeight: '600',
+  },
+});
