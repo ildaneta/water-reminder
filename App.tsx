8e80ddb88063aa94323801fbec89624effeb4725
ildaneta <iildaemanoely@gmail.com>
2022-08-08 15:47:59 -0300

    refactor: usage of routes and pass status bar out of safeAreaView

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
+import Routes from './src/routes';
 
 const App = () => {
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <SafeAreaView style={{backgroundColor: '#fdfdfd', flex: 1}}>
-      <StatusBar barStyle="dark-content" backgroundColor="#fdfdfd" />
-    </SafeAreaView>
+    <>
+      <StatusBar backgroundColor="#fdfdfd" barStyle="dark-content" />
+      {/* eslint-disable-next-line react-native/no-inline-styles */}
+      <SafeAreaView style={{backgroundColor: '#fdfdfd', flex: 1}}>
+        <Routes />
+      </SafeAreaView>
+    </>
   );
 };
 
