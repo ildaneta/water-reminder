39d722d0126f888b7debdc29ca16fccd8957ed2b
ildaneta <iildaemanoely@gmail.com>
2022-08-09 09:31:03 -0300

    feat: insert logic to save input values and call signUp function

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, TouchableOpacity, View} from 'react-native';
 import {StackRoutes} from '../../routes/stack.routes';
 
@@ -12,6 +12,7 @@ import Text from '../../components/Text';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Spacer from '../../components/Spacer';
+import {useAuth} from '../../hooks/auth';
 
 type SignScreenNavigationProp = NativeStackNavigationProp<
   StackRoutes,
@@ -23,6 +24,15 @@ type Props = {
 };
 
 const SignUp = ({navigation}: Props): JSX.Element => {
+  const {signUp, isLoading} = useAuth();
+  const [email, setEmail] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSignUp = () => {
+    signUp({username, email, password});
+  };
+
   return (
     <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
       <View style={styles.containerButtonLinkPassword}>
@@ -43,11 +53,23 @@ const SignUp = ({navigation}: Props): JSX.Element => {
 
         <Spacer top={48} />
 
-        <Input label="Username" placeholder="Username" />
+        <Input
+          label="Username"
+          placeholder="Username"
+          autoCorrect={false}
+          autoCapitalize="none"
+          onChangeText={setUsername}
+        />
 
         <Spacer top={30} />
 
-        <Input label="E-mail" placeholder="E-mail" />
+        <Input
+          label="E-mail"
+          placeholder="E-mail"
+          autoCorrect={false}
+          autoCapitalize="none"
+          onChangeText={setEmail}
+        />
 
         <Spacer top={30} />
 
@@ -55,11 +77,18 @@ const SignUp = ({navigation}: Props): JSX.Element => {
           label="Password"
           placeholder="Password"
           icon={<OpenedEyeSVG />}
+          autoCorrect={false}
+          autoCapitalize="none"
+          onChangeText={setPassword}
         />
 
         <Spacer top={90} />
 
-        <Button label="Create Account" />
+        <Button
+          label="Create Account"
+          onPress={handleSignUp}
+          isLoading={isLoading}
+        />
 
         <Spacer bottom={10} />
       </View>
