14128e73483867bb1b5cab9b4bbd0a636c1c54f4
ildaneta <iildaemanoely@gmail.com>
2022-08-09 01:38:32 -0300

    feat: assemble recovery password screen and its styles

--- src/screens/RecoveryPassword/index.tsx
+++ src/screens/RecoveryPassword/index.tsx
@@ -1,13 +1,54 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {TouchableOpacity, View} from 'react-native';
+import {StackRoutes} from '../../routes/stack.routes';
+
+import HeaderSVG from '../../assets/header.svg';
+import ArrowBackSVG from '../../assets/arrow-back.svg';
 
 import {styles} from './styles';
+import Text from '../../components/Text';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import Spacer from '../../components/Spacer';
 
-const RecoveryPassword = (): JSX.Element => {
+type SignScreenNavigationProp = NativeStackNavigationProp<
+  StackRoutes,
+  'RecoveryPassword'
+>;
+
+type Props = {
+  navigation: SignScreenNavigationProp;
+};
+
+const RecoveryPassword = ({navigation}: Props): JSX.Element => {
   return (
-    <View>
-      <Text />
-    </View>
+    <>
+      <View style={styles.containerButtonLinkPassword}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <ArrowBackSVG />
+        </TouchableOpacity>
+        <Text label="Sign In" style={styles.buttonBackText} />
+      </View>
+
+      <HeaderSVG />
+
+      <View style={styles.container}>
+        <Text style={styles.title} label="Recovery Password" />
+        <Text
+          style={styles.description}
+          label="Don't worry, we'll send you an e-mail to recovery your password."
+        />
+
+        <Spacer top={48} />
+
+        <Input label="E-mail" placeholder="Enter your e-mail" />
+
+        <Spacer top={30} />
+
+        <Button label="Send " />
+      </View>
+    </>
   );
 };
 
--- src/screens/RecoveryPassword/styles.ts
+++ src/screens/RecoveryPassword/styles.ts
@@ -3,6 +3,42 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fdfdfd',
+    backgroundColor: '#f2f2f2',
+    paddingHorizontal: 16,
+    position: 'relative',
+    top: -20,
+  },
+
+  buttonBackText: {
+    fontFamily: 'Baloo2-Medium',
+    fontSize: 18,
+    color: '#222',
+    marginLeft: 16,
+  },
+
+  title: {
+    fontFamily: 'Baloo2-SemiBold',
+    fontSize: 28,
+    color: '#222',
+  },
+
+  description: {
+    fontFamily: 'Baloo2-Regular',
+    fontSize: 18,
+    color: '#222',
+  },
+
+  link: {
+    fontFamily: 'Baloo2-Medium',
+    fontSize: 18,
+    color: '#222',
+    textDecorationLine: 'underline',
+  },
+
+  containerButtonLinkPassword: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 26,
+    paddingLeft: 16,
   },
 });
