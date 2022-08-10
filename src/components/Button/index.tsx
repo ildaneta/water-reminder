0364caf8744856e6542bb017cb49a92c0740d6c1
ildaneta <iildaemanoely@gmail.com>
2022-08-10 05:08:22 -0300

    refactor: remove options to link button and add isLoading prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import {
   View,
   TouchableOpacity,
   TouchableOpacityProps,
-  StyleSheet,
+  ActivityIndicator,
 } from 'react-native';
 import Text from '../Text';
 
@@ -11,50 +11,28 @@ import {styles} from './styles';
 
 interface IButtonProps extends TouchableOpacityProps {
   label: string;
-  type?: 'rectangular' | 'link';
-  color?: 'green' | 'black';
+  isLoading: boolean;
 }
 
-const Button = ({
-  label,
-  type = 'rectangular',
-  color = 'black',
-  ...rest
-}: IButtonProps): JSX.Element => {
-  const customButtonColor = color === 'black' ? '#222' : '#097D6B';
-
-  const externalStyle = StyleSheet.create({
-    buttonLabel: {
-      color: customButtonColor,
-    },
-
-    buttonLink: {
-      borderBottomColor: customButtonColor,
-      borderBottomWidth: 1,
-    },
-  });
+const Button = ({label, isLoading, ...rest}: IButtonProps): JSX.Element => {
   return (
     <>
-      {type === 'rectangular' ? (
-        <View style={styles.container}>
-          <View style={styles.doubleContainer} />
-          <View style={styles.containerButton}>
-            <TouchableOpacity
-              activeOpacity={0.5}
-              style={styles.button}
-              {...rest}>
+      <View style={styles.container}>
+        <View style={styles.doubleContainer} />
+        <View style={styles.containerButton}>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            style={styles.button}
+            disabled={isLoading}
+            {...rest}>
+            {isLoading ? (
+              <ActivityIndicator size="small" color="#fff" />
+            ) : (
               <Text label={label} style={styles.buttonLabel} />
-            </TouchableOpacity>
-          </View>
+            )}
+          </TouchableOpacity>
         </View>
-      ) : (
-        <TouchableOpacity style={externalStyle.buttonLink} {...rest}>
-          <Text
-            style={[styles.buttonLabelLink, externalStyle.buttonLabel]}
-            label={label}
-          />
-        </TouchableOpacity>
-      )}
+      </View>
     </>
   );
 };
