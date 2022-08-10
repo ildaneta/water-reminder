b25cabcb2d7ebf133168a60f6831f1a302cc9e70
ildaneta <iildaemanoely@gmail.com>
2022-08-10 06:08:18 -0300

    feat: implement home and its styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,6 +3,47 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fdfdfd',
+    backgroundColor: '#f4f4f4',
+    paddingTop: 30,
+    paddingHorizontal: 16,
+  },
+
+  containerLoading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  greeting: {
+    fontFamily: 'Baloo2-SemiBold',
+    fontSize: 24,
+    color: '#222',
+  },
+
+  userName: {
+    fontFamily: 'Baloo2-Medium',
+    fontSize: 22,
+    color: '#333',
+  },
+
+  signOut: {
+    fontFamily: 'Baloo2-Medium',
+    fontSize: 10,
+    color: '#F00D0D',
+  },
+
+  containerGreetingSignOut: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  containerIllustration: {
+    alignSelf: 'center',
+    paddingTop: '30%',
+  },
+
+  containerButtonSignOut: {
+    alignItems: 'center',
   },
 });
